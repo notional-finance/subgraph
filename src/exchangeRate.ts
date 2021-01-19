fcbbee72fed88ed4af889425eb910a78d8263f84
Jeff Wu <jeffywu@pm.me>
2021-01-19 07:11:46 -0800

    Updating oracle addresses for underlying aggregators

--- src/exchangeRate.ts
+++ src/exchangeRate.ts
@@ -3,7 +3,20 @@ import { AnswerUpdated } from "../generated/PriceOracle/IAggregator";
 import { PriceOracle, RateValue } from "../generated/schema";
 
 export function handleAnswerUpdated(event: AnswerUpdated): void {
-  let priceOracle = getPriceOracle(event.address);
+  let priceOracle: PriceOracle;
+
+  if (event.address == Address.fromHexString("0xd866a07dea5ee3c093e21d33660b5579c21f140b")) {
+    // DAI / ETH
+    priceOracle = getPriceOracle(Address.fromHexString("0x773616e4d11a78f511299002da57a0a94577f1f4") as Address)
+  } else if (event.address == Address.fromHexString("0x00d02526ca08488342ab634de3b2d0050ecc7f60")) {
+    // USDC / ETH
+    priceOracle = getPriceOracle(Address.fromHexString("0x986b5e1e1755e3c2440e960477f25201b0a8bbd4") as Address)
+  } else if (event.address == Address.fromHexString("0xbd72da70007e47aaf1bbd84918675392cf6885f7")) {
+    // BTC / ETH
+    priceOracle = getPriceOracle(Address.fromHexString("0xdeb288f737066589598e9214e782fa5a8ed689e8") as Address)
+  } else {
+    priceOracle = getPriceOracle(event.address)
+  }
 
   if (priceOracle.exchangeRate == null) {
     // This handles the case where we see an event before the oracle has been configured
